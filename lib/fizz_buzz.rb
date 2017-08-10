27bae899b94f08d7f14a8ac2b39bcb8619b40d57
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-10 20:34:49 +0200

    not sure if I am doing the sad path rigth

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,6 +1,6 @@
 def fizz_buzz(number)
   if has_zero_remainder?(number, 15)
-    'fizz buzz'
+    'fizz_buzz'
   elsif has_zero_remainder?(number, 5)
     'buzz'
   elsif has_zero_remainder?(number, 3)
@@ -8,7 +8,18 @@ def fizz_buzz(number)
   else
     number
   end
+
+else sad_path(number)
+  end
 end
+
+def sad_path(number)
+  if number.is_a? String
+    'no no, you can only put in integers ;)'
+  elsif number.is_a? Float
+    'no no, you can only put in integers ;)'
+  else 'stay positive :)'
+  end
 
 def has_zero_remainder?(number, divider)
   number % divider == 0

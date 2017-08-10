a73d0c368f028aacceed2b6285e82b7e894655eb
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-10 19:30:03 +0200

    fizz_buss3-5

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,8 @@
 def fizz_buzz(number)
   number
+  if number % 3 == 0
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -5,6 +5,10 @@ describe 'fizz_buzz' do
     expect(fizz_buzz(1)).to eq 1
   end
   it "returns 'fizz' if number is divisible by 3" do
-  expect(fizz_buzz(3)).to eq 'fizz'
-end
+     expect(fizz_buzz(3)).to eq 'fizz'
+   end
+  it "returns 'buzz'if number is divisible by 5" do
+    expect(fizz_buzz(5)).to eq 'buzz'
+  end
+
 end

9521bef37df6d910642adaf0f8e7a490eea28638
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-13 20:55:08 +0200

    fixing bugs with the sad path

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,20 +1,22 @@
 def fizz_buzz(number)
 
-  if has_zero_remainder?(number, 15)
-    'fizz_buzz'
-  elsif has_zero_remainder?(number, 5)
-    'buzz'
-  elsif has_zero_remainder?(number, 3)
-    'fizz'
-  elsif number.is_a? String
+  if number.is_a? String
     'no no, you can only put in integers ;)'
-  elsif number.is_a? Float
-    'no no, you can only put in integers ;)'
-  elsif number.is_a? <=0
-      'stay positive :)'
-    end
-  else
-    number
+  # else
+  #   # if has_zero_remainder?(number, 15)
+  #   #   'fizz_buzz'
+  #   # elsif has_zero_remainder?(number, 5)
+  #   #   'buzz'
+  #   # elsif has_zero_remainder?(number, 3)
+  #   #   'fizz'
+  #
+  #   # elsif number.is_a? Float
+  #   #   'no no, you can only put in integers ;)'
+  #   # elsif number <=0
+  #   #   'stay positive :)'
+  #   else
+  #     number
+    # end
   end
 end
 
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -10,16 +10,16 @@ describe 'fizz_buzz' do
   it "returns 'buzz'if number is divisible by 5" do
     expect(fizz_buzz(25)).to eq 'buzz'
   end
-  it "returns 'buzz if number is divisible by 15"do
+  it "returns 'buzz if number is divisible by 15" do
     expect(fizz_buzz(60)).to eq 'fizz_buzz'
   end
-  it "returns 'no no, you can only put in integers ;)' if users puts in string'"do
-    expect(fizz_buzz(String)).to eq 'no no, you can only put in integers ;)'
+  it "returns 'no no, you can only put in integers ;)' if users puts in string'" do
+    expect(fizz_buzz('Hello')).to eq 'no no, you can only put in integers ;)'
   end
-  it "returns 'no no, you can only put in integers ;)'if user puts in float'"do
-    expect(fizz_buzz(Float)).to eq 'no no, you can only put in integers ;)'
-  end
-  it "returns 'stay positive :)' if users puts in a negative number"do
-    expect(fizz_buzz(-1)).to eq 'stay positive :)'
-  end
+  # it "returns 'no no, you can only put in integers ;)'if user puts in float'"do
+  #   expect(fizz_buzz(1,5)).to eq 'no no, you can only put in integers ;)'
+  # end
+  # it "returns 'stay positive :)' if users puts in a negative number"do
+  #   expect(fizz_buzz(-1)).to eq 'stay positive :)'
+  # end
 end

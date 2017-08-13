6bdbf7534d111a9fd09a748dbd94dc7160b07a12
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-13 19:13:38 +0200

    fixing the code

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,27 +1,20 @@
-require pry
-
 def fizz_buzz(number)
+
   if has_zero_remainder?(number, 15)
     'fizz_buzz'
   elsif has_zero_remainder?(number, 5)
     'buzz'
   elsif has_zero_remainder?(number, 3)
     'fizz'
-  else
-    number
-  end
-
-else sad_path(number)
-  end
-end
-
-def sad_path(number)
-  if number.is_a? String
+  elsif number.is_a? String
     'no no, you can only put in integers ;)'
   elsif number.is_a? Float
     'no no, you can only put in integers ;)'
-  else number.is_a? <=0
-     'stay positive :)'
+  elsif number.is_a? <=0
+      'stay positive :)'
+    end
+  else
+    number
   end
 end
 
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -14,12 +14,12 @@ describe 'fizz_buzz' do
     expect(fizz_buzz(60)).to eq 'fizz_buzz'
   end
   it "returns 'no no, you can only put in integers ;)' if users puts in string'"do
-    expect(sad_path(String)).to eq 'no no, you can only put in integers ;)'
+    expect(fizz_buzz(String)).to eq 'no no, you can only put in integers ;)'
   end
   it "returns 'no no, you can only put in integers ;)'if user puts in float'"do
-    expect(sad_path(Float)).to eq 'no no, you can only put in integers ;)'
+    expect(fizz_buzz(Float)).to eq 'no no, you can only put in integers ;)'
   end
   it "returns 'stay positive :)' if users puts in a negative number"do
-    expect(sad_path(-1)).to eq 'stay positive :)'
+    expect(fizz_buzz(-1)).to eq 'stay positive :)'
   end
 end
